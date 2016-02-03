69cf7b4d943325ef389224e5c2ed7783a09cf529
Khaled Ayoubi <khaled.ayoubi@gmail.com>
2016-02-02 17:45:18 -0800

    changed columns service to use the new model

--- columns/src/main/java/com/allocadia/columns/ColumnsController.java
+++ columns/src/main/java/com/allocadia/columns/ColumnsController.java
@@ -3,6 +3,8 @@ package com.allocadia.columns;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.allocadia.domain.Column;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,38 +22,3 @@ public class ColumnsController {
     }
 }
 
-class Column {
-    public Column(int id, String name, String type) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-    }
-
-    private int id;
-    private String name;
-    private String type;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-}
